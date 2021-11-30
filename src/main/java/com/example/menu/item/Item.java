0cb3bf115bd052e6e16e742a9d53b5e3aa18cb03
Luise Loparev <luise.loparev@signavio.com>
2021-11-30 14:04:38 +0100

    add data validation

--- src/main/java/com/example/menu/item/Item.java
+++ src/main/java/com/example/menu/item/Item.java
@@ -1,10 +1,24 @@
 package com.example.menu.item;
 import org.springframework.data.annotation.Id;
 
+import org.hibernate.validator.constraints.URL;
+
+
+import javax.validation.constraints.*;
+
 public class Item {
     private final Long id;
+
+    @NotNull(message = "name is required")
+    @Pattern(regexp="^[a-zA-Z ]+$", message = "name must be a string")
     private final String name;
+
+    @NotNull(message = "price is required")
+    @Positive(message = "price must be positive")
     private final Long price;
+
+    @NotNull(message = "description is required")
+    @Pattern(regexp="^[a-zA-Z ]+$", message = "description must be a string")
     private final String description;
 
     public Item(
